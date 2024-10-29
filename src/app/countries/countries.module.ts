a98aeb4b9c5660c1e4a55f6ae379b04e15237708
Miguel Escoz <miguel.escoz@nter.es>
2024-10-29 13:00:54 +0100

    Tabla con los resultados de búsqueda

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {ByCapitalPageComponent} from './pages/by-capital-page/by-capital-page.component';
 import {ByCountryPageComponent} from './pages/by-country-page/by-country-page.component';
 import {ByRegionComponent} from './pages/by-region/by-region.component';
 import {CountryPageComponent} from './pages/country-page/country-page.component';
 import {CountriesRoutingModule} from './countries-routing.module';
 import {SharedModule} from '../shared/shared.module';
+import {CountryTableComponent} from './components/country-table/country-table.component';
 
 
 @NgModule({
@@ -13,12 +14,14 @@ import {SharedModule} from '../shared/shared.module';
 		ByCapitalPageComponent,
 		ByCountryPageComponent,
 		ByRegionComponent,
-		CountryPageComponent
+		CountryPageComponent,
+		CountryTableComponent
 	],
 	imports: [
 		CommonModule,
 		CountriesRoutingModule,
 		SharedModule,
+		NgOptimizedImage,
 	]
 })
 export class CountriesModule {
