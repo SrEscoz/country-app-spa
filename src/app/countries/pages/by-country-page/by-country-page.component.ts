8b2fec9dcf6def4365c591d182e50a7d9a4a05cf
Miguel Escoz <miguel.escoz@nter.es>
2024-10-29 13:46:21 +0100

    Lógica búsqueda por nombre

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Country} from '../../interfaces/Country';
+import {CountriesService} from '../../services/countries.service';
 
 @Component({
-  selector: 'app-by-country-page',
-  templateUrl: './by-country-page.component.html',
-  styles: ``
+	selector: 'app-by-country-page',
+	templateUrl: './by-country-page.component.html',
+	styles: ``
 })
 export class ByCountryPageComponent {
 
+	private _countries: Country[] = [];
+
+	constructor(private countriesService: CountriesService) {
+	}
+
+	public searchByCountry(country: string): void {
+		this.countriesService.searchByCountry(country)
+			.subscribe(countries => {
+				this._countries = countries;
+			});
+	}
+
+	get countries(): Country[] {
+		return this._countries;
+	}
 }
