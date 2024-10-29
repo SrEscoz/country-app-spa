c7ba9dd9232b48c76a0cb7d69dbfee5fd2997ae6
Miguel Escoz <miguel.escoz@nter.es>
2024-10-29 08:59:07 +0100

    Enrutamiento para los países

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,11 +1,22 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ByCapitalPageComponent} from './pages/by-capital-page/by-capital-page.component';
+import {ByCountryPageComponent} from './pages/by-country-page/by-country-page.component';
+import {ByRegionComponent} from './pages/by-region/by-region.component';
+import {CountryPageComponent} from './pages/country-page/country-page.component';
+import {CountriesRoutingModule} from './countries-routing.module';
 
 
 @NgModule({
-	declarations: [],
+	declarations: [
+		ByCapitalPageComponent,
+		ByCountryPageComponent,
+		ByRegionComponent,
+		CountryPageComponent
+	],
 	imports: [
-		CommonModule
+		CommonModule,
+		CountriesRoutingModule
 	]
 })
 export class CountriesModule {
